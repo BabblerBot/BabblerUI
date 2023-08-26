6d7ca2dd7b4c2a7afd785b25ccc9e955dd725a67
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2023-08-27 01:01:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,18 +13,25 @@ st.set_page_config(page_title='Babbler', page_icon='🤖')
 API_URL = "https://ishvalin-babbler.hf.space/generate"
 
 # App title
-st.title('Babbler')
+st.title('Babbler 🤖')
+
+chat_history = st.empty() 
 
 user_input = st.text_input("Enter a prompt",)
 
-if user_input:
-    response = requests.get(API_URL, params={"query": user_input})
+if st.button("Send"):
+    if user_input:
+        chat_history.append({"sender": "You", "message": user_input})
+        response = requests.get(API_URL, params={"query": user_input})
+
 
     if response.status_code == 200:
         data = response.json()
         output = data["output"]
-        st.markdown(output)
+        chat_history.append({"sender": "Babbler", "message": output})
     else:
         st.markdown("Error: %s" % response.text)
 
-        
+for chat in chat_history:
+    sender = "You" if chat["sender"] == "You" else "Babbler"
+    st.text(f"{sender}: {chat['message']}")
